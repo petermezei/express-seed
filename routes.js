b120166a088d590e397e03fdd98da573d53be49f
mezeipetister2 <mezeipetister@gmail.com>
2017-05-02 16:30:33 +0200

    Initial commit.

---
+++ routes.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'Hello', name: 'Peti' });
+});
+
+router.get('/a', function(req, res, next) {
+  res.render('index', { title: 'Hello', name: 'Peti' });
+});
+
+module.exports = router;
